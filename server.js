7b41d59f9d9be285fedb3c5baeee59929b7f90ee
Gabriel <gabriel-silva_2011@hotmail.com>
2018-10-19 14:21:08 -0300

    criação do models / adicionando dados no mongodb via mongoose

--- server.js
+++ server.js
@@ -1,15 +1,24 @@
 var express = require('express');
 var mongoose = require('mongoose');
+var requireDir = require('require-dir');
 
 //Inicinando o App
 var app = express();
 
 //Inciando o DB
 mongoose.connect('mongodb://localhost:27017/nodeapi', { useNewUrlParser: true });
+requireDir('./src/models');
+
+var Product = mongoose.model('Product');
 
 // Primeira rota
 app.get('/', (req, res) => {
-    res.send('Hello World');
+    Product.create({
+       title: "React Native",
+       description: "Build native apps with React",
+       url: "http://github.com/facebook/react-native" 
+    });
+    return res.send('Hello World');
 });
 
 app.listen(3001);
