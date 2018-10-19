dc171e1ce9d4cc5026cb199a5d7c2011d54d2a85
Gabriel <gabriel-silva_2011@hotmail.com>
2018-10-19 14:05:45 -0300

    instalando nodemon / mongoose

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 var express = require('express');
+var mongoose = require('mongoose');
+
+//Inicinando o App
 var app = express();
 
+//Inciando o DB
+mongoose.connect('mongodb://localhost:27017/nodeapi', { useNewUrlParser: true });
+
+// Primeira rota
 app.get('/', (req, res) => {
     res.send('Hello World');
 });
